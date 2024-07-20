567349d245b964be42d030f0efbd5be87d2234d2
John Grassey <johngrassey@gmail.com>
2024-07-20 18:26:37 -0400

    Add Basic Styling
    Create cards and add colors

--- scripts.js
+++ scripts.js
@@ -1,38 +1,37 @@
 // Library Functions
 
-const myLibrary = [{title: "Harry Potter", author: "JK Rowling"},
-                   {title: "The Bible", author: "God"}
-];
+const myLibrary = [];
 
-// function Book(title, author) {
-//   this.title = title;
-//   this.author = author;
-// };
+function Book(title, author) {
+  this.title = title;
+  this.author = author;
+};
 
-// function addBookToLibrary() {
-//   let title = prompt("Title?");
-//   let author = prompt("Author");
+function addBookToLibrary() {
+  let title = prompt("Title?");
+  let author = prompt("Author");
 
-//   let book1 = new Book(title, author);
-//   myLibrary.push(book1);
-// }
+  let book1 = new Book(title, author);
+  myLibrary.push(book1);
+}
 
-// addBookToLibrary();
-// console.log(myLibrary);
+addBookToLibrary();
+console.log(myLibrary);
 
 // Display Functions
 
 const library = document.querySelector(".library");
 
 function displayBook(books) {
-    for (book of books) {
+    books.map(book => {
         const div = document.createElement("div");
+        div.classList.add("book");
         const p = document.createElement("p");
-        p.textContent = book.title + " by " + book.author;
-
+        p.textContent = `${book.title} by ${book.author}`;
+    
         div.appendChild(p);
         library.appendChild(div);
-    }
+    });
 }
 
 displayBook(myLibrary);
