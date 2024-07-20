fcf9c9e9c4bffc76f2a9c9285a203e9fabf9124c
John Grassey <johngrassey@gmail.com>
2024-07-20 08:13:06 -0400

    Add Function to Display Book Information
    Loop through all books in library. Commented out
    earlier functions to make sure new functions
    working properly.

--- scripts.js
+++ scripts.js
@@ -1,17 +1,38 @@
-const myLibrary = [];
+// Library Functions
 
-function Book(title, author) {
-  this.title = title;
-  this.author = author;
-};
+const myLibrary = [{title: "Harry Potter", author: "JK Rowling"},
+                   {title: "The Bible", author: "God"}
+];
 
-function addBookToLibrary() {
-  let title = prompt("Title?");
-  let author = prompt("Author");
+// function Book(title, author) {
+//   this.title = title;
+//   this.author = author;
+// };
 
-  let book1 = new Book(title, author);
-  myLibrary.push(book1);
+// function addBookToLibrary() {
+//   let title = prompt("Title?");
+//   let author = prompt("Author");
+
+//   let book1 = new Book(title, author);
+//   myLibrary.push(book1);
+// }
+
+// addBookToLibrary();
+// console.log(myLibrary);
+
+// Display Functions
+
+const library = document.querySelector(".library");
+
+function displayBook(books) {
+    for (book of books) {
+        const div = document.createElement("div");
+        const p = document.createElement("p");
+        p.textContent = book.title + " by " + book.author;
+
+        div.appendChild(p);
+        library.appendChild(div);
+    }
 }
 
-addBookToLibrary();
-console.log(myLibrary);
+displayBook(myLibrary);
